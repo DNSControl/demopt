bb4e34479b1fbdefdb1357d8d42c5756a462c2ec
Tom Limoncelli <tlimoncelli@coreweave.com>
2026-07-13 09:06:13 -0400

    demo

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 
-	"codeberg.org/miekg/dns"
+	dnsv2 "codeberg.org/miekg/dns"
 	privatetypes "github.com/DNSControl/demopt/pt"
 	privatetypesrdata "github.com/DNSControl/demopt/pt/rdata"
 )
 
 func main() {
-	rd, _ := dns.NewData(dns.TypeMX, "10 mx.miek.nl.")
+	fmt.Printf("DEBUG: dnsv2.StringToType[%q] = %d\n", "ALIAS", dnsv2.StringToType["ALIAS"])
+	fmt.Printf("DEBUG: dnsv2.TypeToString[%d] = %s\n", privatetypes.TypeALIAS, dnsv2.TypeToString[privatetypes.TypeALIAS])
+
+	rd, _ := dnsv2.NewData(dnsv2.TypeMX, "10 mx.miek.nl.")
 	fmt.Printf("MX: %s\n", rd.String())
 
 	rdalias1 := privatetypesrdata.ALIAS{Target: "foo.com."}
 	fmt.Printf("ALIAS1: %s\n", rdalias1.String())
 
-	rdalias2, _ := dns.NewData(privatetypes.TypeALIAS, "foo.com.")
-	fmt.Printf("ALIAS2: %s\n", rdalias2.String())
-
+	rdalias2, _ := dnsv2.NewData(privatetypes.TypeALIAS, "foo.com.")
+	fmt.Printf("ALIAS2: .String() %s\n", rdalias2.String())
+	fmt.Printf("ALIAS2: (type) %T\n", rdalias2)
 }
